9019a6b47e29d2922d3980339c274fa3a458e22a
Felipe Ferreira <frisney@protonmail.com>
2021-01-31 12:51:54 -0300

    Timer Script

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float timeElapsed;
+
+    public Text text;
+
+    void Start()
+    {
+        timeElapsed = 0f;
+        text = GetComponent<Text>();
+    }
+
+    // Update is called once per fram$
+    void Update()
+    {
+        timeElapsed += Time.deltaTime;
+        
+        text.text = secToMinutes(timeElapsed);
+    }
+
+    string secToMinutes(float seconds)
+    {
+        var min = (Mathf.Floor(seconds/60)).ToString().PadLeft(2,'0');
+        var secs = Mathf.Floor(seconds%60).ToString().PadLeft(2,'0');
+        return $"{min}:{secs}";
+    }
+}
